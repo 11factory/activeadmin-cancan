a22279a4321921f68177e6689f58d9534790c56f
Calvin Yu <calvin@scoutmob.com>
2012-08-05 13:08:19 -0400

    Support more granular permissions

--- lib/activeadmin-cancan/authorization.rb
+++ lib/activeadmin-cancan/authorization.rb
@@ -26,7 +26,7 @@ ActiveAdmin::DSL.class_eval do
     @config = config
     resource = controller.resource_class
     instance_eval do
-      menu :if => proc{ can?(:manage, resource) }
+      menu :if => proc{ can?(:index, resource) }
     end
   end
 
@@ -36,6 +36,57 @@ ActiveAdmin::DSL.class_eval do
       menu :if => proc{ can?(:manage, resource) }
     end
   end
+end
+
+ActiveAdmin::Views::IndexAsTable::IndexTableFor.class_eval do
+  # Adds links to View, Edit and Delete
+  def default_actions(options = {})
+    options = {
+      :name => ""
+    }.merge(options)
+    column options[:name] do |resource|
+      links = ''.html_safe
+      if controller.action_methods.include?('show') and controller.can?(:show, resource.class)
+        links += link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link"
+      end
+      if controller.action_methods.include?('edit') and controller.can?(:edit, resource.class)
+        links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
+      end
+      if controller.action_methods.include?('destroy') and controller.can?(:destroy, resource.class)
+        links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
+      end
+      links
+    end
+  end
+end
+
+ActiveAdmin::Resource::ActionItems.module_eval do
+  private
+    # Adds the default action items to each resource
+    def add_default_action_items
+      # New Link on all actions except :new and :show
+      add_action_item :except => [:new, :show], :if => proc{ can?(:new, controller.resource_class) } do
+        if controller.action_methods.include?('new')
+          link_to(I18n.t('active_admin.new_model', :model => active_admin_config.resource_name), new_resource_path)
+        end
+      end
+
+      # Edit link on show
+      add_action_item :only => :show, :if => proc{ can?(:edit, controller.resource_class) } do
+        if controller.action_methods.include?('edit')
+          link_to(I18n.t('active_admin.edit_model', :model => active_admin_config.resource_name), edit_resource_path(resource))
+        end
+      end
+
+      # Destroy link on show
+      add_action_item :only => :show, :if => proc{ can?(:destroy, controller.resource_class) } do
+        if controller.action_methods.include?("destroy")
+          link_to(I18n.t('active_admin.delete_model', :model => active_admin_config.resource_name),
+            resource_path(resource),
+            :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'))
+        end
+      end
+    end
 end
 
 CanCan::ControllerResource.class_eval do
